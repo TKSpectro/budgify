587cf56b8048f7e9223d7a527ded882b418aff8d
Tom Kaeppler <tomkaeppler@web.de>
2022-02-10 13:52:37 +0100

    implement a basic example gql backend in go

---
+++ backend-go/graph/schema.resolvers.go
@@ -0,0 +1,52 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"budgify-backend-go/graph/generated"
+	"budgify-backend-go/graph/model"
+	"context"
+	"fmt"
+	"math/rand"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	todo := &model.Todo{
+		Text:   input.Text,
+		ID:     fmt.Sprintf("T%d", rand.Int()),
+		UserID: input.UserID,
+	}
+
+	r.todos = append(r.todos, todo)
+	return todo, nil
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	return r.todos, nil
+}
+
+func (r *subscriptionResolver) TodoAdded(ctx context.Context) (<-chan *model.Todo, error) {
+	return r.todosChan, nil
+}
+
+func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
+	return &model.User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+// Subscription returns generated.SubscriptionResolver implementation.
+func (r *Resolver) Subscription() generated.SubscriptionResolver { return &subscriptionResolver{r} }
+
+// Todo returns generated.TodoResolver implementation.
+func (r *Resolver) Todo() generated.TodoResolver { return &todoResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
+type subscriptionResolver struct{ *Resolver }
+type todoResolver struct{ *Resolver }
